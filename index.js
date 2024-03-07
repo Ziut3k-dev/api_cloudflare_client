9ba0a8e7e3f1f5f8bd0e351374c592be3762ca70
Krzysztof Włodarski <wlodarski.dvy@gmail.com>
2024-03-07 15:22:40 +0100

    All to class

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const AccessApplications = require('./lib/resources/AccessApplications');
 const ArgoTunnels = require('./lib/resources/ArgoTunnels');
 const CFIPs = require('./lib/resources/CFIPs');
 const PageRules = require('./lib/resources/PageRules');
+const ZoneSettings = require('./lib/resources/ZoneSettings');
+const User = require('./lib/resources/User');
+const UserTokens = require('./lib/resources/UserTokens');
+const ZoneCustomHostNames = require('./lib/resources/ZoneCustomHostNames');
+const ZoneWorkers = require('./lib/resources/ZoneWorkers');
+const ZoneWorkersRoutes = require('./lib/resources/ZoneWorkersRoutes');
+const ZoneWorkersScript = require('./lib/resources/ZoneWorkersScript');
+const EnterpriseZoneWorkersScripts = require('./lib/resources/EnterpriseZoneWorkersScripts');
+const EnterpriseZoneWorkersRoutes = require('./lib/resources/EnterpriseZoneWorkersRoutes');
+const EnterpriseZoneWorkersKVNamespaces = require('./lib/resources/EnterpriseZoneWorkersKVNamespaces');
+const EnterpriseZoneWorkersKV = require('./lib/resources/EnterpriseZoneWorkersKV');
 
 class Cloudflare {
   constructor(auth) {
@@ -24,6 +35,23 @@ class Cloudflare {
     this.argoTunnels = new ArgoTunnels(this.client);
     this.ips = new CFIPs(this.client);
     this.pageRules = new PageRules(this.client);
+    this.zoneSettings = new ZoneSettings(this.client);
+    this.zoneWorkers = new ZoneWorkers(this.client);
+    this.zoneWorkersRoutes = new ZoneWorkersRoutes(this.client);
+    this.zoneWorkersScript = new ZoneWorkersScript(this.client);
+    this.enterpriseZoneWorkersScripts = new EnterpriseZoneWorkersScripts(
+      this.client
+    );
+    this.enterpriseZoneWorkersRoutes = new EnterpriseZoneWorkersRoutes(
+      this.client
+    );
+    this.enterpriseZoneWorkersKVNamespaces = new EnterpriseZoneWorkersKVNamespaces(
+      this.client
+    );
+    this.enterpriseZoneWorkersKV = new EnterpriseZoneWorkersKV(this.client);
+    this.user = new User(this.client);
+    this.userTokens = new UserTokens(this.client);
+    this.zoneCustomHostNames = new ZoneCustomHostNames(this.client);
   }
 }
 
